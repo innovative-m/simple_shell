aa1b903a3a2b1ee0072d316a52afd49fd62a1fe6
Joseph Tam <tamj963@gmail.com>
2022-06-17 10:30:49 -0700

    remove old main.c

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Simple Shell
 The simple shell promject.
 peer of two
 making use of system calls and three main prototype
--- shell.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char  *argv[])
-{
-  s_loop();
-  return 0;
-}
